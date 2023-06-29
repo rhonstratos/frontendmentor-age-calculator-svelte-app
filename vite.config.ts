ef5853253632d8f6fcdd29ac33c98ea188205a53
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-06-29 17:28:02 +0800

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-    outDir: './docs', // relative to index.html
-    // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
-  }
+	plugins: [sveltekit()]
 });
